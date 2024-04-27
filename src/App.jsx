61cb69d1934018d41f069275df72c094fe72a3b7
paulatalim <paulactalim@gmail.com>
2024-04-27 13:34:29 -0300

    adicao de estilizacao ao status bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Stack from './routes';
 
 const App = () => {
 	return (
 		<SafeAreaView style={{flex: 1}}>
+			<StatusBar
+				backgroundColor="#ffc700"
+			/>
 			<NavigationContainer>
 				<Stack />
 			</NavigationContainer>
